fd570415dc59904b477dde9a8844d1378ed955a6
felixboulin <felixboulin@gmail.com>
2025-06-08 15:29:00 +0200

    ZSH conf.

---
+++ zsh/alias.zsh
@@ -0,0 +1,9 @@
+alias c='clear'
+alias gac='git add . && git commit -m'
+alias tmuxat='tmux attach -t'
+alias vim='nvim'
+alias sessionize='~/bin/tmux-sessionizer.sh'
+alias vt='vim tmp.txt'
+alias d='cd ~/dev'
+alias excel="open -a 'Microsoft Excel'"
+
