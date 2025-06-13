28303d55009c12c3109dded982229b9a747737c4
felixboulin <felixboulin@gmail.com>
2025-06-13 11:29:42 +0200

    zsh

---
+++ zsh/oh-my-zsh-custom/alias.zsh
@@ -0,0 +1,10 @@
+alias c='clear'
+alias gac='git add . && git commit -m'
+alias tmuxat='tmux attach -t'
+alias vim='nvim'
+alias sessionize='~/bin/tmux-sessionizer.sh'
+alias vt='vim tmp.txt'
+alias d='cd ~/dev'
+alias excel="open -a 'Microsoft Excel'"
+alias sublime="open -a 'Sublime Text'"
+
