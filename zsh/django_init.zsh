5fbb0c10c461daeabcbfc8909ed007d6ca1a579b
felixboulin <felixboulin@gmail.com>
2025-07-30 15:34:41 +0200

    FIX: duplicate error

--- zsh/django_init.zsh
+++
@@ -1,8 +0,0 @@
-function init_django_shell() {
-  source ./venv/bin/activate
-  alias m=./manage.py
-  echo "All set, you can now use the 'm' alias to run manage.py commands"
-}
-
-alias di=init_django_shell
-
