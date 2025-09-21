a1b1b66b82995e822ffe664bbed62ccd4fc50434
felix <felixboulin@gmail.com>
2025-09-21 20:02:24 +0200

    rm prev config

--- nvim/after/ftplugin/markdown.lua
+++
@@ -1,9 +0,0 @@
----@diagnostic disable: undefined-global
-
--- auto wrap on insert mode on markdown
-vim.cmd [[
-    setlocal wrapmargin=10
-    setlocal formatoptions+=t
-    setlocal linebreak
-    setlocal spell
-]]
